3aa36d7c8c25bacd0a8c525806a584b60fa9c2a7
eman <emanjr54@gmail.com>
2024-12-01 01:56:26 +0700

    Add files via upload

---
+++ src/assets/Routes/AllProject.js
@@ -0,0 +1,134 @@
+const allProjects = [
+  {
+    title: 'Portofolio Simple',
+    img: require('../img/Project1.webp'),
+    description:
+      'Portofolio simple.',
+    websiteUrl: 'https://portofolio-simple-nine.vercel.app/',
+    githubUrl: 'https://github.com/man296/portofolio-simple',
+    techStack: ['HTML', 'CSS'],
+  },
+  {
+    title: 'linktree lite',
+    img: require('../img/Project17.webp'),
+    description:
+      '-.',
+    websiteUrl: 'https://linktree-wkwk.vercel.app/',
+    githubUrl: 'https://github.com/man296/linktree-apa-ini',
+    techStack: ['HTML'],
+  },
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project16.webp'),
+      //description:
+        //'Comingsoon',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'Comingsoon',
+      //techStack: ['NONE', 'NONE', 'NONE', 'NONE'],
+    //},
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project13.webp'),
+      //description:
+        //'Comingsoon.',
+     // websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE', 'NONE'],
+    //},
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project2.webp'),
+      //description:
+        //'Comingsoon.',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE', 'NONE'],
+    //},
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project3.webp'),
+      //description:
+        //'Comingsoon.',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE'],
+    //},
+
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project15.webp'),
+      //description: 'Comingsoon.',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE'],
+    //},
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project6.webp'),
+      //description:
+        //'Comingsoon.',
+      //maintenence: true,
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE'],
+    //},
+
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project4.webp'),
+      //description:
+        //'Comingsoon',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl:
+        //'https://github.com/man296',
+      //techStack: ['NONE', 'NONE'],
+    //},
+    //{
+      //title: 'Comingsoon',
+      //img: require('../img/Project7.webp'),
+      //description:
+        //'Comingsoon.',
+      //websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE', 'NONE'],
+    //},
+    //{
+     // title: 'Comingsoon',
+      //img: require('../img/Project14.webp'),
+      //description:
+        //'Comingsoon.',
+     // websiteUrl: 'https://raw.githubusercontent.com/man296/comingsoon/refs/heads/main/comingsoon.svg',
+      //githubUrl: 'https://github.com/man296',
+      //techStack: ['NONE', 'NONE', 'NONE', 'NONE'],
+    //},
+  // {
+  //   title: 'Marugame Landing Page',
+  //   img: require('../img/Project11.webp'),
+  //   description: 'Website to promote food menus at marugame udon restaurants',
+  //   websiteUrl: 'https://muhthoriqas.github.io/Marugame-Udon-LandingPage/',
+  //   githubUrl: 'https://github.com/Muhthoriqas/Marugame-Udon-LandingPage',
+  //   techStack: ['Bootstrap 5'],
+  // },
+  // {
+  //   title: 'Fish Weight Prediction',
+  //   img: require('../img/Project12.webp'),
+  //   description:
+  //     'This is a project to detect fish weight using 2 machine learning models, namely linear regression and polynomial regression',
+  //   websiteUrl: '',
+  //   githubUrl:
+  //     'https://github.com/Muhthoriqas/Fish_Weight_Prediction_Linear_And_Polynomial_Regression',
+  //   techStack: ['Python', 'Machine Learning'],
+  // },
+
+  {
+    title: 'Wallet Crypto',
+    img: require('../img/Project5.webp'),
+    description:
+      'Crypto Wallet adalah aplikasi dompet digital yang berguna untuk menyimpan, mengelola, dan melakukan transaksi dengan aset kripto(ini hanya tesnet)',
+    websiteUrl: 'https://wallet-orang-sigma.vercel.app/',
+    githubUrl: 'https://github.com/man296/wallet-orang-sigma',
+    techStack: ['HTML'],
+  },
+];
+
+export default allProjects;
