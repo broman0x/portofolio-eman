1fd70adad09ea277de2f1ab6aa7b30ef97204330
eman <emanjr54@gmail.com>
2024-12-01 02:00:52 +0700

    Add files via upload

---
+++ src/partials/Footer.jsx
@@ -0,0 +1,42 @@
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
+import "./CSS/Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer className="px-4">
+        <div className="foot-left d-flex">
+          <p>Sulaeman &copy; 2024</p>
+        </div>
+        <div className="foot-right d-flex">
+          <a
+            href="https://github.com/man296"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="foot-icon" />
+            &nbsp;&nbsp;GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/eman-pedia-b6862b33b/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin className="foot-icon" />
+            &nbsp;&nbsp;Linkedin
+          </a>
+          <a
+            href="https://www.instagram.com/emann_0x/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillInstagram className="foot-icon" />
+            &nbsp;&nbsp;Instagram
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
